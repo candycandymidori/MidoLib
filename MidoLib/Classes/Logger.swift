db7fb1f0927d8112d2dac88b0f1637c024a30412
midori <midori@candyhouse.co>
2022-11-07 12:04:05 +0800

    cc

---
+++ MidoLib/Classes/Logger.swift
@@ -0,0 +1,18 @@
+import Foundation
+import os.log
+
+public class lg {
+  public static func mido(_ items: Any..., file: String = #file, function: String = #function,
+         line: Int = #line, tag: String = "WEB mido =>") {
+    #if DEBUG
+    let fileName = (file as NSString).lastPathComponent
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "HH:mm:ss"
+    dateFormatter.timeZone = TimeZone(abbreviation: "Asia/Taipei")
+    dateFormatter.locale = Locale(identifier: "ja_JP")
+    let ismain = (Thread.isMainThread) ? "主線":"副線"
+    let content = "\(tag) \(dateFormatter.string(from: Date())) \(items) \(fileName):\(line) \(ismain)"
+    print(content)
+    #endif
+  }
+}
